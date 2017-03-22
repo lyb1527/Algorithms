15f89c72ef36083a0e92fbb9e0b122172509478c
Yingbo <yingbo_liu@brown.edu>
2017-03-22 20:03:11 +0800

    adding topological sorting

--- topological-sorting.py
+++ topological-sorting.py
@@ -1,22 +1,35 @@
-# Definition of a directed graph node
-# class DirectedGraphNode:
-#    def __init__(self, x):
-#        self.label = x
-#        self.neighbors = []
+'''
+A topological sort takes a directed acyclic graph and produces a linear ordering of all its vertices such that tif the graph G contains an edge(v,w) then the vertex v comes before the vertex w in the ordering. DAG are used in many applications to indicate the precedence of events.
+
+The topological sort is a simple but useful adaption of a depth first search. The ALgo for the topological sort is as follows:
+
+1. call dfs(g) for some graph g. The main reason we want to call depth first search is to compute the finish times for each of the vertices.
+2. Store the vertices in a list in decreasing order of finish time
+3. return the ordered list as the result of the topological sort
+
+'''
+
+
+
+
+# Definition for a Directed graph node
+class DirectedGraphNode:#
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
 
 class Solution:
     def dfs(self, i, countrd, ans):
         ans.append(i)
-        countrd -= 1
+        countrd[i] -= 1
         for j in i.neighbors:
             countrd[j] = countrd[j] - 1
             if countrd[j] == 0:
                 self.dfs(j, countrd, ans)
-
-    '''
-    @param graph: a list of directed graph node
-    @return: a list of integers
-    '''
+    """
+    @param graph: A list of Directed graph node
+    @return: A list of integer
+    """
     def topSort(self, graph):
         countrd = {}
         for x in graph:
@@ -31,3 +44,22 @@ class Solution:
             if countrd[i] == 0:
                 self.dfs(i, countrd, ans)
         return ans
+
+sol=Solution()
+A=DirectedGraphNode('0')
+B=DirectedGraphNode('1')
+C=DirectedGraphNode('2')
+D=DirectedGraphNode('3')
+E=DirectedGraphNode('4')
+F=DirectedGraphNode('5')
+
+A.neighbors=[B,C,D]
+B.neighbors=[E]
+C.neighbors=[E,F]
+D.neighbors=[E,F]
+E.neighbors=[]
+F.neighbors=[]
+graph=[A,B,C,D,E,F]
+
+for i in sol.topSort(graph):
+    print i.label
